7520f00e4826aeac72b5be1ec206c300db453c94
ckim328 <35876108+ckim328@users.noreply.github.com>
2018-04-10 13:54:50 -0400

    Alert Controller and FXML Alert

---
+++ Birds/src/birds/AlertController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package birds;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author Christine Kim
+ */
+public class AlertController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+     
+    @FXML public Label error;
+   
+    public void setAlertText(String text) {
+        // set text from another class
+        error.setText(text);
+    } 
+
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+      
+    }    
+    //have a textbox that displays the error when needed
+    
+    
+}
