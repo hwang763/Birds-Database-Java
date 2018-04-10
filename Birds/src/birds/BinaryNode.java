8b89285a7cd0bc9501eb1ef96793d159a23f9200
ckim328 <35876108+ckim328@users.noreply.github.com>
2018-04-10 14:19:18 -0400

    fixed spacing

--- Birds/src/birds/BinaryNode.java
+++ Birds/src/birds/BinaryNode.java
@@ -13,69 +13,85 @@ public class BinaryNode <DataKey> {
     private DataKey data;
 private BinaryNode < DataKey > left;
 private BinaryNode < DataKey > right;
+
 public BinaryNode () {
-this (null); // call next constructor
+    this (null); // call next constructor
 } // end default constructor
+
 public BinaryNode (DataKey dataPortion) {
-this (dataPortion, null, null); // call next constructor
+    this (dataPortion, null, null); // call next constructor
 } // end constructor
+
 public BinaryNode (DataKey dataPortion, BinaryNode < DataKey > leftChild, BinaryNode < DataKey > rightChild) {
-data = dataPortion;
-left = leftChild;
-right = rightChild;
+    data = dataPortion;
+    left = leftChild;
+    right = rightChild;
 } // end constructor
+
 public DataKey getData () {
-return data;
+    return data;
 } // end getData
+
 public void setData (DataKey newData) {
-data = newData;
+    data = newData;
 } // end setData
+
 public BinaryNode < DataKey > getLeftChild () {
-return left;
+    return left;
 } // end getLeftChild
+
 public void setLeftChild (BinaryNode < DataKey > leftChild) {
-left = (BinaryNode < DataKey > ) leftChild;
+    left = (BinaryNode < DataKey > ) leftChild;
 } // end setLeftChild
+
 public boolean hasLeftChild () {
-return left != null;
+    return left != null;
 } // end hasLeftChild
+
 public boolean isLeaf () {
-return (left == null) && (right == null);
+    return (left == null) && (right == null);
 } // end isLeaf
+
 public BinaryNode < DataKey > getRightChild () {
-return left;
+    return left;
 } // end getLeftChild
+
 public void setRightChild (BinaryNode < DataKey > rightChild) {
-right = (BinaryNode < DataKey > ) rightChild;
+    right = (BinaryNode < DataKey > ) rightChild;
 } // end setLeftChild
+
 public boolean hasRightChild () {
-return right!= null;
+    return right!= null;
 } // end hasLeftChild
+
 public BinaryNode < DataKey > copy () {
-BinaryNode < DataKey > newRoot = new BinaryNode < DataKey > (data);
-if (left != null)
-newRoot.left = (BinaryNode < DataKey > ) left.copy ();
-if (right != null)
-newRoot.right = (BinaryNode < DataKey > ) right.copy ();
-return newRoot;
+    BinaryNode < DataKey > newRoot = new BinaryNode < DataKey > (data);
+    if (left != null)
+        newRoot.left = (BinaryNode < DataKey > ) left.copy ();
+    if (right != null)
+        newRoot.right = (BinaryNode < DataKey > ) right.copy ();
+    return newRoot;
 } // end copy
+
 public int getHeight () {
-return getHeight (this); // call private getHeight
+    return getHeight (this); // call private getHeight
 } // end getHeight
+
 private int getHeight (BinaryNode < DataKey > node) {
-int height = 0;
-if (node != null)
-height = 1 + Math.max (getHeight (node.left), getHeight (node.right));
-return height;
+    int height = 0;
+    if (node != null)
+        height = 1 + Math.max (getHeight (node.left), getHeight (node.right));
+    return height;
 } // end getHeight
+
 public int getNumberOfNodes () {
-int leftNumber = 0;
-int rightNumber = 0;
-if (left != null)
-leftNumber = left.getNumberOfNodes ();
-if (right != null)
-rightNumber = right.getNumberOfNodes ();
-return 1 + leftNumber + rightNumber;
+    int leftNumber = 0;
+    int rightNumber = 0;
+    if (left != null)
+        leftNumber = left.getNumberOfNodes ();
+    if (right != null)
+        rightNumber = right.getNumberOfNodes ();
+    return 1 + leftNumber + rightNumber;
 } // end getNumberOfNodes 
 
 }
